5ecbcf81eca1285fb61300e85657c1d303b21b16
AbderrhmanAbdellatif <36771724+AbderrhmanAbdellatif@users.noreply.github.com>
2018-03-01 02:40:19 +0300

    yeni

--- PythonApplication3-string/PythonApplication3-string/PythonApplication3_string.py
+++ PythonApplication3-string/PythonApplication3-string/PythonApplication3_string.py
@@ -2,9 +2,12 @@
 #print(x)
 #x=x+2
 #print(x)
-x=5
-if x<10:
-    print("smaller")
-if x > 20:
-    print("bigerr")
-print("finis")
+#x=5
+#if x<10:
+ #   print("smaller")
+#if x > 20:
+#    print("bigerr")
+#print("finis")
+#print('C:\some\name')   # \n yeni satıra geçiş yapacaktır
+#print(r'C:\some\name')  # metin öncesi r kullanıldığında \n gördüğünde bir alt satıra geçmeyecektir
+#print (30 * 'like -->' +  'egypt') 
