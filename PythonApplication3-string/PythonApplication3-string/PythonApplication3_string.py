2842297623e065b32bff989ff5f952b3bb400a51
AbderrhmanAbdellatif <36771724+AbderrhmanAbdellatif@users.noreply.github.com>
2018-03-01 12:40:13 +0300

    yeni

--- PythonApplication3-string/PythonApplication3-string/PythonApplication3_string.py
+++ PythonApplication3-string/PythonApplication3-string/PythonApplication3_string.py
@@ -11,3 +11,82 @@
 #print('C:\some\name')   # \n yeni satıra geçiş yapacaktır
 #print(r'C:\some\name')  # metin öncesi r kullanıldığında \n gördüğünde bir alt satıra geçmeyecektir
 #print (30 * 'like -->' +  'egypt') 
+#omer ="Egypt"
+#Abde="cici"
+#print(omer+" : "+Abde)
+#text=("istanbulda kar yagiyor ")
+#print (text)
+#a=5
+#a=str(a)
+#print(type(a))
+#b=int(3.5)
+#print(type(b))
+#no= float('12!!345.0')
+#print(type(no))
+#word_python="AaaEgypt"
+#print(word_python[0])
+#print(word_python[1])
+#print(word_python[2])
+#print(word_python[-3])
+#print(word_python[-2])
+#Art="fsmvu"
+#print("Ar"+Art[2:]+" ====>  "+len(Art))
+#print(Art[0:3])
+
+
+#i=0
+#while i!=100:
+#    i=i+1
+#    print(i)
+#print("while bitti")
+
+#input int icin sayi=int(sayi)
+#sayi =input("lutfen bir sayi giriniz  :=>  ");
+#sayi=float(sayi) , sayi =int(sayi)
+#if sayi<100:
+#   print("bu sayi 100den azdir ")
+#if sayi>100:
+#    print("bu sayi 100den buyuktur")
+#print("porgram  bitti ")
+
+#Hour=input("Enter Hours : ")
+#Hour =int(Hour)
+#rate=input("Enter Rate : ")
+#rate =float(rate)
+#print("Pay : ",(Hour * rate))
+
+#x= input("x ===> ")
+#x=int(x)
+#if x <2 :
+#    print('small')
+#elif x<10:
+#    print('Medium')
+#else:
+#    print('LARGE')
+#print('All done')
+
+
+#astr ='Bob' 
+#try:
+#    print("Hello")
+#    iste=int(astr)
+#except:
+#    print('error')
+#try:
+# a=input("enter Hours = >")
+# a=int(a)
+# b=input("enter Rate = >")
+# b=int(b)
+#except:
+#    print("Error ,please enter numeric input")
+
+#while True:
+#    line=input(' > ')
+#    if line=='done':
+#      break
+#    print(line)
+#print('Done ! ')
+
+#for i in [5,4,3,2,1] :
+#    print(i)
+#print('done')
